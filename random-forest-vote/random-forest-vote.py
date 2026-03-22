602cd617a679c504c49d429107a5d643d8bb9ed1
huiling-y <45547647+huiling-y@users.noreply.github.com>
2026-03-22 21:26:30 +0100

    Update solution for random-forest-vote

--- random-forest-vote/random-forest-vote.py
+++ random-forest-vote/random-forest-vote.py
@@ -5,35 +5,17 @@ def random_forest_vote(predictions):
     Compute the majority vote from multiple tree predictions.
     """
     # Write code here
-    predictions_by_sample = np.array(predictions).T
-
+    
     pred = []
 
-    for _pred in predictions_by_sample:
+    for _pred in zip(*predictions):
         cls, counts = np.unique(_pred, return_counts=True)
 
-        if len(set(counts)) == len(cls):
-            pred.append(cls[np.argmax(counts)])
-        else:
-            paired = list(zip(counts, cls))
-            sorted_pairs = sorted(paired)[::-1]
+        max_count = max(counts)
 
-            if len(paired) == 1:
-                pred.append(cls[0])
-            else:
-                largest_count = sorted_pairs[0][0]
+        pred.append(cls[counts == max_count][0])
 
-                if sorted_pairs[1][0] != largest_count:
-                    pred.append(sorted_pairs[0][1])
-                else:
-                    candidates = []
 
-                    for _count,_cls in sorted_pairs:
-                        if _count == largest_count:
-                            candidates.append(_cls)
-                        else:
-                            break
-                    pred.append(sorted(candidates)[0])
                     
     return pred
                         
