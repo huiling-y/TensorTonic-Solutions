58ff9d2f73c72c8bb8e2ae66973f77ce3383d62e
huiling-y <45547647+huiling-y@users.noreply.github.com>
2026-04-09 00:50:18 +0200

    Add solution for k-means-assignment

---
+++ k-means-assignment/k-means-assignment.py
@@ -0,0 +1,21 @@
+def k_means_assignment(points, centroids):
+    """
+    Assign each point to the nearest centroid.
+    """
+    # Write code here
+    N = len(points)
+    D = len(points[0])
+    assignment = [-1] * N
+
+    for ip, p in enumerate(points):
+        idx = -1
+        MIN = float('inf')
+
+        for ic, c in enumerate(centroids):
+            distance = sum((p[d]-c[d])**2 for d in range(D))
+            if distance < MIN:
+                idx = ic
+                MIN = distance
+        assignment[ip] = idx 
+    return assignment
+        
